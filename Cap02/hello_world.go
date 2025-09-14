f9e660907c99966ac17a90caa700b3bcd424de79
Arrudxx <d4niel.arruda@gmail.com>
2025-09-14 19:21:38 -0300

    Primeiro Commit sobre aula hello world e operador curto

---
+++ Cap02/hello_world.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	_, erros := fmt.Println("Hello World!")
+	fmt.Println(erros)
+
+}
---
+++ Cap02/operador_curto_de_declaracao.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//Uso:
+	// Operador de Declaração: := (e só pode ser usando dentro de um codeblock)
+	// Operador de Atribuição: = )(para atribuir valores a variáveis já existentes)
+
+	// x := 10
+	// y := "Olá bom dia"
+	// fmt.Printf("x: %v, %T\n", x, x)
+	// fmt.Printf("x: %v, %T\n\n", y, y)
+
+	// x, z := 20, 30
+	// fmt.Printf("x: %v, %T\n", x, x)
+	// fmt.Printf("z: %v, %T\n", z, z)
+
+	// Terminologia:
+
+	x := 10 < 20
+
+	fmt.Println(x)
+
+}
