fccd7bf0d31e9d54ef1cf9eaa44a1183c7f4a0dd
Arrudxx <d4niel.arruda@gmail.com>
2025-10-25 14:18:02 -0300

    Entendendo struct em go

---
+++ Cap10/structs.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type cliente struct {
+	nome      string
+	sobrenome string
+	fumante   bool
+}
+
+func main() {
+	cliente1 := cliente{
+		nome:      "Daniel",
+		sobrenome: "Arruda",
+		fumante:   false,
+	}
+
+	cliente2 := cliente{"Joana", "Pereira", true}
+
+	fmt.Println(cliente1)
+	fmt.Println(cliente2)
+}
---
+++ Cap10/structs_embutidos.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type pessoa struct {
+	nome  string
+	idade int
+}
+
+type profissional struct {
+	pessoa
+	titulo  string
+	salario int
+}
+
+func main() {
+	pessoa1 := pessoa{
+		nome:  "Alfredo",
+		idade: 30,
+	}
+
+	pessoa2 := profissional{
+		pessoa: pessoa{
+			nome:  "Daniel",
+			idade: 31,
+		},
+		titulo:  "Pizzaiolo",
+		salario: 10000,
+	}
+
+	fmt.Println(pessoa1)
+	fmt.Println(pessoa2)
+	fmt.Println(pessoa1.idade)
+	fmt.Println(pessoa2.pessoa.nome)
+
+}
